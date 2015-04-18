23857476c58e5137141825cbd9b984eef8da2913
Jordanbana <jordanbana@gmail.com>
2015-04-18 12:50:40 -0700

    SQL code for music

--- CS201_Project/src/MusicLibrary.java
+++ CS201_Project/src/MusicLibrary.java
@@ -6,6 +6,11 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioInputStream;
@@ -21,140 +26,72 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.ResultSetMetaData;
+
 public class MusicLibrary extends JFrame {
 	public static final long serialVersionUID = 1;
 	
-	//should make these private and add getters and setters soon
-	Clip clip;
-	URL[] urlArray;
-	JLabel albumIconLabel;
 	
-	
-	//these are the album covers
-	public static final String BASE_URL_PATH = "http://jbwebsitebuilder.com"  +
-	        "/Java_Music_File/";
-	public static final String[] URL_PATHS = {
-		      "20120928010410!Headlines.png",
-		      "Childish-gambino-because-the-internet.gif",
-		   };
-	
-	//testing
-	
-	
-	//change song method to go to the next song
-	public void changeSong() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
-		AudioInputStream ais = AudioSystem.
-                getAudioInputStream( urlArray[1] );
-            clip.open(ais);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                }
-            });
-            //should also change the album cover here eventually
+	//fix this cause its bad coding style
+	Connection connection;
+	String dburl;
+	String userName;
+	String passWord;
+	Connection conn;
+
+	private void connect(){
+		connection = null;
+		dburl = "jdbc:mysql://104.236.176.180:3306/cs201";
+		userName = "cs201";
+		passWord = "manishhostage";
+
+			 try {
+				 Class.forName("com.mysql.jdbc.Driver");
+				 conn = DriverManager.getConnection("jdbc:mysql://104.236.176.180/cs201", "cs201", "manishhostage");
+				 } catch (ClassNotFoundException e) {
+				 e.printStackTrace();
+				 } catch (SQLException e) {
+				 e.printStackTrace();
+				 }
 	}
-	
-	
-	//change song method to go to the PREVIOUS one
-	public void previousSong() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
-		AudioInputStream ais = AudioSystem.
-                getAudioInputStream( urlArray[0] );
-            clip.open(ais);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                }
-            });
-            //should also change the album cover here eventually
-	}
-	
+
 	
 	
 	public MusicLibrary() throws Exception{
 		super("MUSIC");
 		setSize(500,600);
 		setLocation(500,0);
+		connect();
 		
-		//ARRAY of TYPE URL TO HAVE MORE THAN 1 song
-		urlArray = new URL[2];
-        //code for music
-        URL url = new URL(
-                "http://jbwebsitebuilder.com/Java_Music_File/Headlines.wav");
-        URL url2 = new URL(
-                "http://jbwebsitebuilder.com/Java_Music_File/3005.wav");
-
-        urlArray[0] = url;
-        urlArray[1] = url2;
-        
-        clip = AudioSystem.getClip();
-        
-        JPanel mainJpanel = new JPanel();
-        //JButton, image, and action listener for stop Music
-		JButton stopButton = new JButton("");//"STOP MUSIC");
-		URL stopButtonImageurl = new URL("http://jbwebsitebuilder.com/Java_Music_File/stop.png");
-        BufferedImage stopButtonImg = ImageIO.read(stopButtonImageurl);
-        ImageIcon stopButtonIcon = new ImageIcon(stopButtonImg);
-        stopButton.setIcon(stopButtonIcon);
-		stopButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e){
-				clip.stop();
+		try{
+			Statement st = conn.createStatement();
+			String queryCheck = "SELECT * from music_table"; //WHERE song_name";
+			PreparedStatement ps = (PreparedStatement) conn.prepareStatement(queryCheck);
+			ResultSet rs = ps.executeQuery();
+			java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			while (rs.next()){
+				for (int i = 1; i <= columnsNumber; i++){
+					if (i > 1){
+						System.out.print(", ");
+					}
+					String columnValue = rs.getString(i);
+					System.out.println(columnValue + " " + rsmd.getColumnName(i));
+				}
+				System.out.println("");
 			}
-		});
-		
-		
-        //Button and action listener for resume Music
-		JButton startButton = new JButton("");//"RESUME MUSIC");
-		URL startButtonImageurl = new URL("http://jbwebsitebuilder.com/Java_Music_File/play.png");
-        BufferedImage startButtonImg = ImageIO.read(startButtonImageurl);
-        ImageIcon startButtonIcon = new ImageIcon(startButtonImg);
-        startButton.setIcon(startButtonIcon);
-		startButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e){
-				clip.start();
-			}
-		});
-		
-		
-		
-		albumIconLabel = new JLabel("");
-		//trying to display album cover
-		String fullUrlPath = BASE_URL_PATH + URL_PATHS[0];
-		try {
-            URL imageurl = new URL(fullUrlPath);
-            BufferedImage img = ImageIO.read(imageurl);
-            Image ResizedImage = img.getScaledInstance(475, 475,  java.awt.Image.SCALE_SMOOTH);
-            ImageIcon icon = new ImageIcon(ResizedImage);
-            albumIconLabel.setIcon(icon);
-            albumIconLabel.setSize(475,475);
-    		mainJpanel.add(albumIconLabel);
-    		mainJpanel.add(stopButton);
-    		mainJpanel.add(startButton);
-            //JOptionPane.showMessageDialog(null, icon);
-         } catch (MalformedURLException e) {
-            e.printStackTrace();
-         }
-		
-		//adding components to the JFRAME
-		add(mainJpanel);
-		
-        
-            // getAudioInputStream() also accepts a File or InputStream
-            AudioInputStream ais = AudioSystem.
-                getAudioInputStream( urlArray[0] );
-            clip.open(ais);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    // A GUI element to prevent the Clip's daemon Thread
-                    // from terminating at the end of the main()
-                    //JOptionPane.showMessageDialog(null, "Close to exit!");
-                }
-            });
-		
-		setVisible(true);
+			//System.out.println(rs.absolute(1));
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//System.out.println(ps.toString());
+			//ALO
+
+		}catch(Exception E){
+			E.printStackTrace();
+			
+		}
+	
+
+
 	}
 }
