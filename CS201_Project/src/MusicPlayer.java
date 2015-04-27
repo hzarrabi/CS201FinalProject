fd2c015ec9fe5a9bb60027d82a7f9d5184e0dddb
Jordanbana <jordanbana@gmail.com>
2015-04-26 22:42:28 -0700

    LEAP MOTION WORKS MFer WITH HOOMANS HELP!!!!

--- CS201_Project/src/MusicPlayer.java
+++ CS201_Project/src/MusicPlayer.java
@@ -29,6 +29,10 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import java.lang.Math;
+
+import com.leapmotion.leap.*;
+import com.leapmotion.leap.Gesture.State;
 import com.mysql.jdbc.PreparedStatement;
 
 
@@ -126,6 +130,11 @@ public class MusicPlayer extends JPanel{
 		if (myThread != null)
 			myThread.suspend();
 	}	
+	public static void resumeThread()
+	{
+		if (myThread != null)
+			myThread.resume();
+	}
 	
 	private void initializeComponents(){
 		this.setSize(dim);
@@ -775,9 +784,10 @@ public class MusicPlayer extends JPanel{
 			public void actionPerformed(ActionEvent e) {
 				//musicObject.resumeSong();
 				if (myThread == null){
+					//LEAP MOTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 					myThread = musicObject.playTheSong();
-					//Sample LeapMotionThread = new Sample(myThread);
-					//LeapMotionThread.run();
+					Sample leap = new Sample();
+					leap.start();
 					if (beingPlayed) {}
 					else {
 						try
@@ -1221,4 +1231,132 @@ public class MusicPlayer extends JPanel{
 		add(mainPanel, BorderLayout.NORTH);
 		//add(tabPanel, BorderLayout.SOUTH);
 	}
+	///////////////========================================================================START
+	class SampleListener extends Listener {
+	    public void onInit(Controller controller) {
+	        System.out.println("Initialized");
+	    }
+
+	    public void onConnect(Controller controller) {
+	        System.out.println("Connected");
+	        controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+	        controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+	        controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
+	        controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
+	    }
+
+	    public void onDisconnect(Controller controller) {
+	        //Note: not dispatched when running in a debugger.
+	        System.out.println("Disconnected");
+	    }
+
+	    public void onExit(Controller controller) {
+	        System.out.println("Exited");
+	    }
+	    public void onFrame(Controller controller) {
+	        // Get the most recent frame and report some basic information
+	        Frame frame = controller.frame();
+	        System.out.println("Frame id: " + frame.id()
+	                         + ", timestamp: " + frame.timestamp()
+	                         + ", hands: " + frame.hands().count()
+	                         + ", fingers: " + frame.fingers().count()
+	                         + ", tools: " + frame.tools().count()
+	                         + ", gestures " + frame.gestures().count());
+
+
+	        GestureList gestures = frame.gestures();
+	        
+	        if(frame.gestures().count()>0){
+	        	stopThread();
+	        	System.out.println("FOUND A GESTURE");
+	        }
+	        
+	        
+	        for (int i = 0; i < gestures.count(); i++) {
+	            Gesture gesture = gestures.get(i);
+
+	            switch (gesture.type()) {
+	                case TYPE_CIRCLE:
+	                    CircleGesture circle = new CircleGesture(gesture);
+
+	                    // Calculate clock direction using the angle between circle normal and pointable
+	                    String clockwiseness;
+	                    if (circle.pointable().direction().angleTo(circle.normal()) <= Math.PI/4) {
+	                        // Clockwise if angle is less than 90 degrees
+	                        clockwiseness = "clockwise";
+	                    } else {
+	                        clockwiseness = "counterclockwise";
+	                    }
+
+	                    // Calculate angle swept since last frame
+	                    double sweptAngle = 0;
+	                    if (circle.state() != State.STATE_START) {
+	                        CircleGesture previousUpdate = new CircleGesture(controller.frame(1).gesture(circle.id()));
+	                        sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Math.PI;
+	                    }
+
+	                    System.out.println("Circle id: " + circle.id()
+	                               + ", " + circle.state()
+	                               + ", progress: " + circle.progress()
+	                               + ", radius: " + circle.radius()
+	                               + ", angle: " + Math.toDegrees(sweptAngle)
+	                               + ", " + clockwiseness);
+	                    break;
+	                case TYPE_SWIPE:
+	                    SwipeGesture swipe = new SwipeGesture(gesture);
+	                    System.out.println("Swipe id: " + swipe.id()
+	                               + ", " + swipe.state()
+	                               + ", position: " + swipe.position()
+	                               + ", direction: " + swipe.direction()
+	                               + ", speed: " + swipe.speed());
+	                    resumeThread();
+	                    break;
+	                case TYPE_SCREEN_TAP:
+	                    ScreenTapGesture screenTap = new ScreenTapGesture(gesture);
+	                    System.out.println("Screen Tap id: " + screenTap.id()
+	                               + ", " + screenTap.state()
+	                               + ", position: " + screenTap.position()
+	                               + ", direction: " + screenTap.direction());
+	                    break;
+	                case TYPE_KEY_TAP:
+	                    KeyTapGesture keyTap = new KeyTapGesture(gesture);
+	                    System.out.println("Key Tap id: " + keyTap.id()
+	                               + ", " + keyTap.state()
+	                               + ", position: " + keyTap.position()
+	                               + ", direction: " + keyTap.direction());
+	                    break;
+	                default:
+	                    System.out.println("Unknown gesture type.");
+	                    break;
+	            }
+	        }
+
+	        if (!frame.hands().isEmpty() || !gestures.isEmpty()) {
+	            System.out.println();
+	        }
+	    }
+	}
+	//////////////=========================================================================END
+	class Sample extends Thread{
+	    public void run() {
+	        // Create a sample listener and controller
+	        SampleListener listener = new SampleListener();
+	        Controller controller = new Controller();
+
+	        // Have the sample listener receive events from the controller
+	        controller.addListener(listener);
+
+	        // Keep this process running until Enter is pressed
+	        System.out.println("Press Enter to quit...");
+	        try {
+	            System.in.read();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        // Remove the sample listener when done
+	        controller.removeListener(listener);
+	    }
+	}
+	
 }
